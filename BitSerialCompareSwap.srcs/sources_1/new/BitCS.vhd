daad17d59dc849fce1a76103468c24a06b2c2f0c
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2022-03-10 18:30:25 +0100

    Reduced Bitserial CS to two LUTs in implementation

--- BitSerialCompareSwap.srcs/sources_1/new/BitCS.vhd
+++ BitSerialCompareSwap.srcs/sources_1/new/BitCS.vhd
@@ -28,8 +28,8 @@ use IEEE.NUMERIC_STD.all;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library UNISIM;
+use UNISIM.VComponents.all;
 
 entity BitCS is
   port (
@@ -41,7 +41,9 @@ entity BitCS is
     start : in  std_logic);
 end BitCS;
 
+
 architecture Behavioral of BitCS is
+
   component MUX_PRIMITIVE is
     port (
       a   : in  std_logic;
@@ -54,35 +56,76 @@ architecture Behavioral of BitCS is
   signal state : std_logic_vector(1 downto 0) := "00";
 
 begin
-
-  StateMachine : process(CLK, start)
-  begin
-    if(CLK'event and CLK = '1') then
-      if start = '1' then
-        state <= "00";
-      else
-        case state is
-          when "00" =>
-            if a = '1' and b = '0' then
-              state <= "01";
-            elsif a = '0' and b = '1' then
-              state <= "10";
-            else
-              state <= "00";
-            end if;
-          when "01" =>
-            state <= "01";
-          when "10" =>
-            state <= "10";
-          when "11" => 
-            state <= "00";
-        end case;
-      end if;
-    end if;
-  end process;
-
   outMUX : MUX_PRIMITIVE
     port map(a, b, state(1), c, d);
+
+-- Truthtable of State Machine for BitCS
+-- For more details see ug953
+-- | I5 |    I4 | I3 | I2 | I1 | I0 |     O6 |     O5 | INIT | INIT |
+-- |    | start | Q1 | Q0 |  b |  a | Q1_n+1 | Q0_n+1 |   O6 |   O5 |
+-- |----+-------+----+----+----+----+--------+--------+------+------|
+-- |  1 |     0 |  0 |  0 |  0 |  0 |      0 |      0 |   32 |    0 |
+-- |  1 |     0 |  0 |  0 |  0 |  1 |      0 |      1 |   33 |    1 |
+-- |  1 |     0 |  0 |  0 |  1 |  0 |      1 |      0 |   34 |    2 |
+-- |  1 |     0 |  0 |  0 |  1 |  1 |      0 |      0 |   35 |    3 |
+-- |----+-------+----+----+----+----+--------+--------+------+------|
+-- |  1 |     0 |  0 |  1 |  0 |  0 |      0 |      1 |   36 |    4 |
+-- |  1 |     0 |  0 |  1 |  0 |  1 |      0 |      1 |   37 |    5 |
+-- |  1 |     0 |  0 |  1 |  1 |  0 |      0 |      1 |   38 |    6 |
+-- |  1 |     0 |  0 |  1 |  1 |  1 |      0 |      1 |   39 |    7 |
+-- |----+-------+----+----+----+----+--------+--------+------+------|
+-- |  1 |     0 |  1 |  0 |  0 |  0 |      1 |      0 |   40 |    8 |
+-- |  1 |     0 |  1 |  0 |  0 |  1 |      1 |      0 |   41 |    9 |
+-- |  1 |     0 |  1 |  0 |  1 |  0 |      1 |      0 |   42 |   10 |
+-- |  1 |     0 |  1 |  0 |  1 |  1 |      1 |      0 |   43 |   11 |
+-- |----+-------+----+----+----+----+--------+--------+------+------|
+-- |  1 |     0 |  1 |  1 |  0 |  0 |      0 |      0 |   44 |   12 |
+-- |  1 |     0 |  1 |  1 |  0 |  1 |      0 |      0 |   45 |   13 |
+-- |  1 |     0 |  1 |  1 |  1 |  0 |      0 |      0 |   46 |   14 |
+-- |  1 |     0 |  1 |  1 |  1 |  1 |      0 |      0 |   47 |   15 |
+-- |----+-------+----+----+----+----+--------+--------+------+------|
+-- |  1 |     1 |  0 |  0 |  0 |  0 |      0 |      0 |   48 |   16 |
+-- |  1 |     1 |  0 |  0 |  0 |  1 |      0 |      0 |   49 |   17 |
+-- |  1 |     1 |  0 |  0 |  1 |  0 |      0 |      0 |   50 |   18 |
+-- |  1 |     1 |  0 |  0 |  1 |  1 |      0 |      0 |   51 |   19 |
+-- |  1 |     1 |  0 |  1 |  0 |  0 |      0 |      0 |   52 |   20 |
+-- |  1 |     1 |  0 |  1 |  0 |  1 |      0 |      0 |   53 |   21 |
+-- |  1 |     1 |  0 |  1 |  1 |  0 |      0 |      0 |   54 |   22 |
+-- |  1 |     1 |  0 |  1 |  1 |  1 |      0 |      0 |   55 |   23 |
+-- |  1 |     1 |  1 |  0 |  0 |  0 |      0 |      0 |   56 |   24 |
+-- |  1 |     1 |  1 |  0 |  0 |  1 |      0 |      0 |   57 |   25 |
+-- |  1 |     1 |  1 |  0 |  1 |  0 |      0 |      0 |   58 |   26 |
+-- |  1 |     1 |  1 |  0 |  1 |  1 |      0 |      0 |   59 |   27 |
+-- |  1 |     1 |  1 |  1 |  0 |  0 |      0 |      0 |   60 |   28 |
+-- |  1 |     1 |  1 |  1 |  0 |  1 |      0 |      0 |   61 |   29 |
+-- |  1 |     1 |  1 |  1 |  1 |  0 |      0 |      0 |   62 |   30 |
+-- |  1 |     1 |  1 |  1 |  1 |  1 |      0 |      0 |   63 |   31 |
+
+
+-- => O5 = "0000 0000 0000 0000 0000 0000 1111 0010" = X"000000F2"
+-- => O6 = "0000 0000 0000 0000 0000 1111 0000 0100" = X"00000F04"
+-- => INIT = X"00000F04000000F2"
+
+  LUT6_2_inst : LUT6_2
+    generic map (
+      INIT => X"00000F04000000F2")      -- Specify LUT Contents
+    port map (
+      O5 => state(0),                          -- 5-LUT output (1-bit)
+      O6 => state(1),                          -- 6/5-LUT output (1-bit)
+      I0 => a,
+      -- LUT input (1-bit)
+      I1 => b,
+      -- LUT input (1-bit)
+      I2 => state(0),
+      -- LUT input (1-bit)
+      I3 => state(1),
+      -- LUT input (1-bit)
+      I4 => start,
+      -- LUT input (1-bit)
+      I5 => '1'
+     -- LUT input (1-bit)
+      );
+
 
 
 end Behavioral;
