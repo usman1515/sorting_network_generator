238dca1dba134b7d850d49e8df02733443bbb818
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2022-03-10 11:54:09 +0100

    Moved MUX from process to component.
    Also changed the states from named states to states with custom encoding.

---
+++ BitSerialCompareSwap.srcs/sources_1/new/2x2MUX.vhd
@@ -0,0 +1,58 @@
+----------------------------------------------------------------------------------
+-- Company:
+-- Engineer:
+--
+-- Create Date: 03/08/2022 02:46:11 PM
+-- Design Name:
+-- Module Name: BitCS - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.all;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MUX is
+  port (
+    a   : in std_logic;
+    b   : in std_logic;
+    sel : in std_logic;
+    
+    c : out std_logic;
+    d : out std_logic);
+end MUX;
+
+architecture Behavioral of MUX is
+begin
+
+  process (a, b, sel)
+  begin  -- process
+    if sel = '1' then
+      c <= b;
+      d <= a;
+    else
+      c <= a;
+      d <= b;
+    end if;
+  end process;
+
+end architecture Behavioral;
--- BitSerialCompareSwap.srcs/sources_1/new/BitCS.vhd
+++ BitSerialCompareSwap.srcs/sources_1/new/BitCS.vhd
@@ -20,11 +20,11 @@
 
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.all;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,65 +32,57 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity BitCS is
-    Port (
-        a : in std_logic;
-        b : in std_logic;
-        c : out std_logic;
-        d : out std_logic;
-        CLK : in std_logic;
-        R : in std_logic);
+  port (
+    a     : in  std_logic;
+    b     : in  std_logic;
+    c     : out std_logic;
+    d     : out std_logic;
+    CLK   : in  std_logic;
+    start : in  std_logic);
 end BitCS;
 
 architecture Behavioral of BitCS is
-    TYPE STATE_TYPE IS (EQUAL, INIT, LARGER, SMALLER);
-    SIGNAL state   : STATE_TYPE := INIT;
+  component MUX is
+    port (
+      a   : in  std_logic;
+      b   : in  std_logic;
+      sel : in  std_logic;
+      c   : out std_logic;
+      d   : out std_logic);
+  end component MUX;
+
+  signal state : std_logic_vector(1 downto 0) := "00";
 
 begin
 
-    StateMachine: process(CLK, R)
-    begin
-        if(CLK'EVENT AND CLK= '1') THEN
-            if R= '1' THEN
-                state <= INIT;
+  StateMachine : process(CLK, start)
+  begin
+    if(CLK'event and CLK = '1') then
+      if start = '1' then
+        state <= "00";
+      else
+        case state is
+          when "00" =>
+            if a = '1' and b = '0' then
+              state <= "01";
+            elsif a = '0' and b = '1' then
+              state <= "10";
             else
-                case state is
-                    when INIT=>
-                        if a='1' AND b = '0' then
-                            state <= LARGER;
-                        elsif a='0' AND b ='1' then
-                            state <= SMALLER;
-                        else
-                            state <= EQUAL;
-                        end if;
-                    when EQUAL=>
-                        if a='1' AND b = '0' then
-                            state <= LARGER;
-                        elsif a='0' AND b ='1' then
-                            state <= SMALLER;
-                        else
-                            state <= EQUAL;
-                        end if;
-                    when LARGER=>
-                        state <= LARGER;
-                    when SMALLER=>
-                        state <= SMALLER;
-                end case;
+              state <= "00";
             end if;
-        end if;
-    end process;
+          when "01" =>
+            state <= "01";
+          when "10" =>
+            state <= "10";
+          when "11" => 
+            state <= "00";
+        end case;
+      end if;
+    end if;
+  end process;
 
-
-    MUX: process
-    begin
-        wait until rising_edge(CLK);
-        if state = SMALLER then
-            c <= b;
-            d <= a;
-        else
-            c <= a;
-            d <= b;
-        end if;
-    end process;
+  outMUX : MUX
+    port map(a, b, state(1), c, d);
 
 
 end Behavioral;
