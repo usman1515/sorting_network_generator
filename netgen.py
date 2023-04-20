d1b68379d0cb2e8657081b48bc5be8e8a332cf5a
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2023-04-20 14:47:27 +0200

    Added timestamps to console output.

--- netgen.py
+++ netgen.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import numpy as np
 import math
 import fire
+import time
 
 from scripts import vhdl
 import scripts.network_generators as generators
@@ -31,17 +32,30 @@ def get_templates(path=Path()):
     return templates
 
 
+def print_timestamp(title: str):
+    time_str = "[%b %d %H:%M:%S]: "
+    print(time.strftime(time_str) + title, end="")
+
+
 class Interface:
     def __init__(self):
+        self.start_time = time.perf_counter_ns()
         self.entities = dict()
+        print_timestamp("Parsing sources...")
         self.entities = get_sources(Path("src/"))
+        print(" done.")
         self.templates = dict()
+        print_timestamp("Parsing templates...")
         self.templates = get_templates(Path("templates/"))
+        print(" done.")
         self.generator = None
         self.network = None
         self.template = None
         self.ffreplacements = []
         self.reporter = Reporter()
+
+    def __del__(self):
+        print("Finished after " + str(time.perf_counter_ns() - self.start_time) + "ns.")
 
     def __str__(self):
         return ""
@@ -100,38 +114,51 @@ class Interface:
         return self
 
     def generate(self, ntype: str, N: int, SW: int = 1):
+        valid_types = ["oddeven", "bitonic", "blank"]
         if "oddeven" == ntype.lower():
+            print_timestamp("Generating Odd-Even-Network...")
             logp = int(math.ceil(math.log2(N)))
             self.generator = generators.OddEven()
             self.network = self.generator.create(2**logp)
             self.network = self.generator.reduce(self.network, N)
         elif "bitonic" == ntype.lower():
+            print_timestamp("Generating Bitonic-Network...")
             logp = int(math.ceil(math.log2(N)))
             self.generator = generators.Bitonic()
             self.network = self.generator.create(N)
             self.network = self.generator.reduce(self.network, N)
         elif "blank" == ntype.lower():
+            print_timestamp("Generating blank Network...")
             logp = int(math.ceil(math.log2(N)))
             depth = logp * (logp + 1) // 2
             self.network = generators.Network(N, depth)
         else:
             print("Options: oddeven, bitonic, blank")
+        if ntype.lower() in valid_types:
+            print(" done.")
         return self
 
     def distribute_signal(self, signal_name: str, max_fanout: int):
         """Distributes the signal with the given name so that
         only a number of rows less than max_fanout is driven by the signal.
         """
+        print_timestamp("Distributing signal '{}'...".format(signal_name))
         if self.network:
             self.network = self.generator.distribute_signal(
                 self.network, signal_name, max_fanout
             )
+        print(" done.")
         return self
 
     def shape(self, shape_type, num_outputs):
         if shape_type.lower() not in ["max", "min", "median"]:
             print("Error: shape_type options are max, min, median")
         else:
+            print_timestamp(
+                "Reshaping Network to {} with {} outputs...".format(
+                    shape_type, num_outputs
+                ),
+            )
             N = self.network.get_N()
             if shape_type.lower() == "max":
                 self.generator.prune(self.network, range(0, num_outputs))
@@ -144,11 +171,16 @@ class Interface:
                 upper_bound = N // 2 + (num_outputs + 1) // 2
                 self.generator.prune(self.network, range(lower_bound, upper_bound))
                 self.network.shape = "median"
+            print(" done.")
         return self
 
     def prune(self, output_list):
+        print_timestamp(
+            "Pruning Network outputs...",
+        )
         self.generator.prune(self.network, output_list)
         self.network.shape = "mixed"
+        print(" done.")
         return self
 
     def show_network(self):
@@ -162,11 +194,15 @@ class Interface:
             print("No template selected.")
         return self
 
-    def replace_ff(self, entity, limit=1500, entity_ff=48):
+    def replace_ff(self, entity: str, limit=1500, entity_ff=48):
+        print_timestamp(
+            "Replacing FF with {} resource...".format(entity),
+        )
         entity_obj = self.entities[entity]
         ralloc = Block_Allocator()
         ffrepl = ralloc.reallocate_ff(self.network, entity_obj, limit, entity_ff)
         self.ffreplacements.append(ffrepl)
+        print(" done.")
         return self
 
     def write_template(
@@ -176,6 +212,9 @@ class Interface:
         cs: str = "SWCS",
         W: int = 8,
     ):
+        print_timestamp(
+            "Writing template {} ...".format(template_name),
+        )
         self.template = self.templates[template_name]
         cs = self.entities[cs]
         if not path:
@@ -196,6 +235,7 @@ class Interface:
             **kwargs,
         )
         self.reporter.add(self.network)
+        print(" done.")
         print("Wrote {}".format(path))
         return self
 
